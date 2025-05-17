7223cff2993dc53bceb461f9c4522553ce034f98
naitik-popli <natik9588@gmail.com>
2025-05-17 14:40:21 +0530

    Updated configuration files and server setup

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ require("./config/dbConnect"); // Database connection
 
 const http = require("http");
 const app  = require("./app/app");
-const userRoutes = require('./routes/academics/examRoutes');
-const userRoutes = require('./routes/academics/examResultsRoutes');
-const userRoutes = require('./routes/academics/questionRoutes');
-const userRoutes = require('./routes/staff/adminRouter');
-const userRoutes = require('./routes/staff/teacherRouter');
-const userRoutes = require('./routes/students/studentRouter');
-const PORT = process.env.PORT  || 2020;
+const examRoutes = require('./routes/academics/examRoutes');
+const examResultsRoutes = require('./routes/academics/examResultsRoutes');
+const questionRoutes = require('./routes/academics/questionRoutes');
+const adminRouter = require('./routes/staff/adminRouter');
+const teacherRouter = require('./routes/staff/teacherRouter');
+const studentRouter = require('./routes/students/studentRouter');
+
+app.use('/exams', examRoutes);
+app.use('/exam-results', examResultsRoutes);
+app.use('/questions', questionRoutes);
+app.use('/admin', adminRouter);
+app.use('/teachers', teacherRouter);
+app.use('/students', studentRouter);
+
+const PORT = process.env.PORT || 2020;
 
 // server
 const server = http.createServer(app);
