3bea35012e9bbae418ecd9302d1af3ee9dafd067
naitik-popli <natik9588@gmail.com>
2025-05-17 14:31:39 +0530

    Updated configuration files and server setup

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ require("./config/dbConnect"); // Database connection
 
 const http = require("http");
 const app  = require("./app/app");
+const userRoutes = require('./routes/academics/examRoutes');
+const userRoutes = require('./routes/academics/examResultsRoutes');
+const userRoutes = require('./routes/academics/questionRoutes');
+const userRoutes = require('./routes/staff/adminRouter');
+const userRoutes = require('./routes/staff/teacherRouter');
+const userRoutes = require('./routes/students/studentRouter');
 const PORT = process.env.PORT  || 2020;
 
 // server
